b7d5f0cc73fa84ec8f77bcce884ca0c908d0aa2a
David Rowland <drowaudio@github.com>
2010-07-28 12:39:44 +0000

    Library: Updating tree structure.

---
+++ dRowAudio/src/maths/dRowAudio_BezierCurve.cpp
@@ -0,0 +1,16 @@
+/*
+ *  dRowAudio_BezierCurve.cpp
+ *  dRowAudio
+ *
+ *  Created by David Rowland on 22/01/2010.
+ *  Copyright 2010 dRowAudio. All rights reserved.
+ *
+ */
+
+#include "../core/dRowAudio_StandardHeader.h"
+
+BEGIN_DROWAUDIO_NAMESPACE
+
+#include "dRowAudio_BezierCurve.h"
+
+END_DROWAUDIO_NAMESPACE
