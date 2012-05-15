87be53afc1d9b8cd83da310d785e460c1924fb2f
drowland <drowland96@gmail.com>
2012-05-15 01:46:06 +0100

    Updated demo app. Fixed some MSVC warnings.

---
+++ apps/dRowAudio Demo/Source/DemoHeader.h
@@ -0,0 +1,19 @@
+/*
+  ==============================================================================
+
+    DemoHeader.h
+    Created: 13 May 2012 1:39:40am
+    Author:  David Rowland
+
+  ==============================================================================
+*/
+
+#ifndef __DEMOHEADER_H_F6E7C8DE__
+#define __DEMOHEADER_H_F6E7C8DE__
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+using namespace drow;
+
+
+#endif  // __DEMOHEADER_H_F6E7C8DE__
