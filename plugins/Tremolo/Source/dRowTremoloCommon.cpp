fc4e0a71a8187551b79668dc6b4c52cab11cff42
drowland <drowland96@gmail.com>
2012-06-12 01:27:11 +0100

    Plugins: Updated Tremolo to use a new, reusable style. Added some common files to accommodate this.

--- plugins/Tremolo/Source/dRowTremoloCommon.cpp
+++
@@ -1,9 +0,0 @@
-#include "includes.h"
-#include "dRowTremoloCommon.h"
-
-
-char* TremoloInterface::Parameters::Names[]		= TremoloInterfaceParameterNames;
-//char* TremoloInterface::Meters::Names[]			= TremoloInterfaceMeterNames;
-//char* TremoloInterface::Buttons::Names[]		= TremoloInterfaceButtonNames;
-//char* TremoloInterface::MenuOptions::Names[]	= TremoloInterfaceMenuOptionNames;
-//char* TremoloInterface::MenuOptions::Label		= TremoloInterfaceMenuOptionsLabel;
