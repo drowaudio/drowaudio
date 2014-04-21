76a7298f077f568b5eb3eec67ab2e0093fa9f3b6
dRowAudio <dave@drowaudio.co.uk>
2014-04-21 16:38:46 +0100

    iOS warning fixes.

--- dRowAudio/native/dRowAudio_AVAssetAudioFormat.mm
+++ dRowAudio/native/dRowAudio_AVAssetAudioFormat.mm
@@ -175,7 +175,7 @@ public:
                                                  &dataPointer);
                     if (bufferRef != NULL)
                     {
-                        const int samplesExpected = CMSampleBufferGetNumSamples (sampleRef);
+                        const int samplesExpected = (int) CMSampleBufferGetNumSamples (sampleRef);
                               
                         const int numSamplesNeeded = fifoBuffer.getNumAvailable() + (samplesExpected * numChannels);
                         if (numSamplesNeeded > fifoBuffer.getSize()) //*** need to keep existing
