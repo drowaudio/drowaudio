103d7af20a9b60a704d4e443e5eb6625aea6d869
atiliomf <81830200+atiliomf@users.noreply.github.com>
2023-01-02 17:45:18 +0100

    Update dRowAudio_Spectrograph.cpp

--- module/dRowAudio/gui/dRowAudio_Spectrograph.cpp
+++ module/dRowAudio/gui/dRowAudio_Spectrograph.cpp
@@ -29,7 +29,7 @@
   ==============================================================================
 */
 
-#if DROWAUDIO_USE_FFTREAL
+#if DROWAUDIO_USE_FFTREAL || DROWAUDIO_USE_VDSP
 
 Spectrograph::Spectrograph (int fftSizeLog2)
     : fftEngine         (fftSizeLog2),
